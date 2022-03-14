7a5c742e98a32aa4b0c0f345a5cb2fd88477241c
DIDRICH Damien <89045920+skunkoff1@users.noreply.github.com>
2022-03-14 11:58:29 +0100

    add password regex / user register done

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -1,10 +1,13 @@
 <?php
 $message = '';
-$error = '';
+$valid = true;
 $userName;
 $userPassword;
 $userPassword2;
 $userMail;
+$PASSWORD_REGEX = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/";
+
+require('./connect.php');
 
 if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['password2'])) {
     $userName = htmlspecialchars($_POST['name']);
@@ -13,19 +16,96 @@ if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password']) 
     $userPassword2 = htmlspecialchars($_POST['password2']);
 
     if(strlen($userName)<1) {
-        header('location: ../index.php?name='.$userName.'&email='.$userMail.'&error=true&messageName=true');
+        $valid = false;
+        header('location: ../index.php?error=true&messageName=true');
         exit();
     }
 
     if(strlen($userMail)<1) {
-        header('location: ../index.php?name='.$userName.'&email='.$userMail.'&error=true&messageEmail=true');
+        $valid = false;
+        header('location: ../index.php?error=true&messageEmail=true');
         exit();
+    }
+
+    //VERIFICATION SI L'EMAIL EXISTE DEJA DANS LA BASE DE DONNEES
+
+    $req = $db->prepare('SELECT COUNT(*) AS x FROM users WHERE email = ?');
+    $req->execute(array($userMail));
+
+    while ($result = $req->fetch()) {
+
+        if ($result['x'] != 0) {
+            $valid = false;
+            header('location: ../index.php?error=true&usedEmail=true');
+            exit();
+        }
+    }
+
+    //VERIFICATION SI LE PSEUDO EXISTE DEJA DANS LA BASE DE DONNEES
+
+    $req = $db->prepare('SELECT COUNT(*) AS x FROM users WHERE username = ?');
+    $req->execute(array($userName));
+
+    while ($result = $req->fetch()) {
+
+        if ($result['x'] != 0) {
+            $valid = false;
+            header('location: ../index.php?error=true&usedName=true');
+            exit();
+        }
     }
 
     if(!filter_var($userMail, FILTER_VALIDATE_EMAIL)) {
-        header('location: ../index.php?name='.$userName.'&email='.$userMail.'&error=true&messageInvalidEmail=true');
+        $valid = false;
+        header('location: ../index.php?error=true&messageInvalidEmail=true');
         exit();
     }
 
-    header('location: ../index.php?name=&&messageSuccess=true');
+    if(strlen($userPassword)<1) {
+        $valid = false;
+        header('location: ../index.php?error=true&messagePassword=true');
+        exit();
+    }
+    
+    if(preg_match($PASSWORD_REGEX, $userPassword)==0) {
+        $valid = false;
+        header('location: ../index.php?error=true&messageInvalidPassword=true');
+        exit();
+    }
+
+    if(strlen($userPassword2)<1) {
+        $valid = false;
+        header('location: ../index.php?error=true&messagePassword2=true');
+        exit();
+    }
+
+    if($userPassword != $userPassword2) {
+        $valid = false;
+        header('location: ../index.php?&error=true&messageNotEqualsPassword=true');
+        exit();
+    }
+
+
+
+    if($valid) {
+        
+         //SI OK, CHIFFRAGE DU MOT DE PASSE
+
+        $salt = time() . sha1($userMail);
+        $password = $salt . sha1($userPassword . "123") . "25";
+
+
+        // SI OK, CREATION DU SECRET (correspond à l'id du cookie)
+
+        $secret = sha1($userMail) . time();
+        $secret = sha1($secret) . time();
+
+        $req = $db->prepare('INSERT INTO users(username, email, secret, password, salt) VALUES (?,?,?,?,?)');
+        $req->execute(array($userName, $userMail, $secret, $password, $salt));
+
+        header('location: ../index.php?messageSuccess=true');
+    }
+    
+
+    
 }
--- views/indexView.php
+++ views/indexView.php
@@ -19,7 +19,7 @@
             <label for="password">Votre mot de passe</label>
             <input type="password" name="password">
             <label for="password2">Confirmez le mot de passe</label>
-            <input type="password2" name="password2">
+            <input type="password" name="password2">
             <button type="submit">Soumettre</button>
         </form>
 
@@ -41,9 +41,45 @@
         </div>
         <?php } ?>
 
+        <?php if (isset($_GET['error']) && isset($_GET['messagePassword'])) { ?>
+        <div class="alert error">
+            <p>Veuillez renseigner votre mot de passe</p>
+        </div>
+        <?php } ?>
+
+        <?php if (isset($_GET['error']) && isset($_GET['messagePassword2'])) { ?>
+        <div class="alert error">
+            <p>Veuillez confirmer votre mot de passe</p>
+        </div>
+        <?php } ?>
+
+        <?php if (isset($_GET['error']) && isset($_GET['usedEmail'])) { ?>
+        <div class="alert error">
+            <p>L'email est déjà utilisé</p>
+        </div>
+        <?php } ?>
+
+        <?php if (isset($_GET['error']) && isset($_GET['usedName'])) { ?>
+        <div class="alert error">
+            <p>Le nom ou pseudo est déjà utilisé</p>
+        </div>
+        <?php } ?>
+
+        <?php if (isset($_GET['error']) && isset($_GET['messageInvalidPassword'])) { ?>
+        <div class="alert error">
+            <p>Le mot de passe doit contenir 8 caractères, au moins 1 majuscule et au moins 1 chiffre</p>
+        </div>
+        <?php } ?>
+
+        <?php if (isset($_GET['error']) && isset($_GET['messageNotEqualsPassword'])) { ?>
+        <div class="alert error">
+            <p>Les mots de passe sont différents</p>
+        </div>
+        <?php } ?>
+
         <?php if (isset($_GET['messageSuccess'])) { ?>
         <div class="alert success">
-            <p>Vous êtes maintenant inscrit. <a href="../index.php">Connectez vous</a>.</p>
+            <p>Vous êtes maintenant inscrit. <a href="../login.php">Connectez vous</a>.</p>
         </div>
         <?php } ?>
 
